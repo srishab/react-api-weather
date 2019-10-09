0c2ecb5af22b52cab8ae3e545e5436902631a998
srishab <iamrishab19@gmail.com>
2019-10-10 01:54:25 +0530

    changed a file

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,8 @@ class Form extends React.Component {
   render() {
     return (
       <form onSubmit={this.props.getWeather}>
-        <input type='text' name='city' placeholder='city...' />
-        <input type='text' name='country' placeholder='country...' />
+        <input type='text' name='city' placeholder='Enter city...' />
+        <input type='text' name='country' placeholder='Enter country...' />
         <button>get weather</button>
       </form>
     );
